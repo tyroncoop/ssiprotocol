587c245ab8ae3cbbaf21d4852c5a4fa477d97249
Mariana Gaddi <majin@ssiprotocol.com>
2023-03-28 17:07:06 -0300

    segunda escritura de traslate

--- pages/about/index.tsx
+++ pages/about/index.tsx
@@ -17,7 +17,7 @@ const Component: NextPage = () => {
                 <br />
                 <div className="content1">
                     {t(
-                        'Tyron originates from a critical and conscious view of the damage that some technologies (their human utility) cause to nature. The founder of the SSI Protocol graduated in petroleum engineering, and during his professional career, he learned about the effects of this extractive industry. His experience allowed him to understand that behind the exploitation of natural resources lies a logic of domination and commodification of nature for the economic benefit of a few people.'
+                        'about1'
                     )}
                     <br />
                     <br />
@@ -31,7 +31,7 @@ const Component: NextPage = () => {
                     <br />
                     <br />
                     {t(
-                        "TYRON, a decentralized network of Self-sovereign identities, was created in these circumstances. The word tyron derives from the Greek turannos, meaning 'sovereign'. Thus, the purpose of the TYRON Network is to give you sovereignty over your data. Sovereignty to empower the world and improve our security, with no need to exploit nature to do so, by using new Web3 technologies."
+                        "about2"
                     )}
                     <br />
                     <br />
@@ -45,12 +45,12 @@ const Component: NextPage = () => {
                     <br />
                     <br />
                     {t(
-                        'The TYRON Network enables people to manage their digital identities, proving who they are without intermediaries. This functionality gets achieved by anchoring Decentralized Identifiers (DIDs) on blockchain platforms as a shared root of trust. With these technologies, we can have legitimacy as users without handing over the control of our data to a centralized authority or third parties. With TYRON, you can become the owner of your identity and financial assets.'
+                        'about3'
                     )}
                     <br />
                     <br />
                     {t(
-                        'The administration of the project is carried out by the Tyron Coop, a decentralized autonomous organization (DAO).'
+                        'about4'
                     )}
                 </div>
             </div>
